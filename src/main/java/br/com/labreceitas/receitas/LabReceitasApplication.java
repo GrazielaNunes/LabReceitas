f45ff967253c8317d3617d4a96dff5548fdacdf1
grazi <grazielanunesj@gmail.com>
2023-05-21 14:48:50 -0300

    Criada configuração inicial e estrutura MVC

---
+++ src/main/java/br/com/labreceitas/receitas/LabReceitasApplication.java
@@ -0,0 +1,13 @@
+package br.com.labreceitas.receitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabReceitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabReceitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/labreceitas/receitas/LabReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.labreceitas.receitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabReceitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
